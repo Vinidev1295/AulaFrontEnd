391da79f453319a7e4b3815858bc43e5447062e4
Vinidev1295 <vinidias1295@gmail.com>
2024-10-16 10:44:42 -0300

    Create logica.js

---
+++ logica.js
@@ -0,0 +1,13 @@
+// Função para converter o valor inserido
+function converter(){
+    // Pegando o valor inserido pelo usuário
+    const valor = document.getElementById("valor").value
+    // Pegando a moeda escolhida pelo usuário
+    const moeda = document.getElementById("moeda").value
+    // Taxas de conversão
+    const taxas = {
+        "USD": 0.17,
+        "EUR": 0.16,
+        "GBP": 0.14
+    }
+}
